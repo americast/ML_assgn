c6bd8170af357c928a1444244ab1a0d867728273
americast <sayan.sinha@iitkgp.ac.in>
2019-02-11 02:32:31 +0530

    Update

--- Assgn1/main.py
+++ Assgn1/main.py
@@ -1,48 +1,59 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-degree = 5
-max_iter = 50
-alpha = 0.05
+MAX_ITER = 500
+ALPHA = 0.05
+NUM_DATA = 100
 
-X = np.random.uniform(0, 1, 10)
+X = np.random.uniform(0, 1, NUM_DATA)
 
 
-noise = np.random.normal(0, 0.3, 10)
+noise = np.random.normal(0, 0.3, NUM_DATA)
 
 Y = (2 * np.pi * X) + noise
 
-# indices = range(10)
-# choice_1 = random.choice(indices)
-# indices.remove(choice_1)
-# choice_2 = random.choice(indices)
+cutoff = int(0.8 * NUM_DATA)
 
-X_test = X[8:]
-Y_test = Y[8:]
+X_test = X[cutoff:]
+Y_test = Y[cutoff:]
 
-X_train = X[:8]
-Y_train = Y[:8]
+X_train = X[:cutoff]
+Y_train = Y[:cutoff]
+
+np.save("X_train", X_train)
+np.save("X_test", X_test)
+np.save("Y_train", Y_train)
+np.save("Y_test", Y_test)
+
+for DEGREE in xrange(1, 10):
+	print DEGREE
 
 
-W = np.random.uniform(0, 1, degree + 1)
+
+	W = np.random.uniform(0, 1, DEGREE + 1)
 
 
-for i in xrange(max_iter):
+	for i in xrange(MAX_ITER):
 
-	cost = 0.0
-	sum_ = 0.0
-	for m in xrange(8):
-		X = X_train[m]
-		Y = Y_train[m]
-		X_power = np.array([X**j for j in xrange(degree+1)])
+		cost = 0.0
+		sum_ = 0.0
+		for m in xrange(cutoff):
+			X = X_train[m]
+			Y = Y_train[m]
+			X_power = np.array([X**j for j in xrange(DEGREE+1)])
 
-		Y_pred = np.dot(W, X_power)
-		sum_ += (Y_pred - Y)
-		cost += (Y - Y_pred)**2
+			Y_pred = np.dot(W, X_power)
+			sum_ += (Y_pred - Y)
+			cost += (Y - Y_pred)**2
 
-	cost/= (2 * m)
-	grad = sum_ * X_power
+		cost/= (2 * (m+1))
+		grad = sum_ * X_power
 
-	W -= (alpha/(degree + 1)) * grad
-	print("Cost at iter "+str(i)+"/"+str(max_iter)+": "+str(cost))
+		W -= (ALPHA/(DEGREE + 1)) * grad
+		print("Cost at iter "+str(i)+"/"+str(MAX_ITER)+": "+str(cost))
+
+
+	np.save("W_"+str(DEGREE), W)
+
+
 
