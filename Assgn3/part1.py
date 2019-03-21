1d0777f73f7c9898a34425cb6c64129a88b04721
americast <sayan.sinha@iitkgp.ac.in>
2019-03-22 02:41:05 +0530

    Update

--- Assgn3/part1.py
+++ Assgn3/part1.py
@@ -7,13 +7,11 @@ def jaccard_coef(a, b):
 	return float(inter) / union
 
 def distance(clust_a, clust_b, linkage="complete"):
-	i = 0
-	j = 0
 	max_ = float('-inf')
 	min_ = float('inf')
 	for i in range(len(clust_a)):
 		for j in range(len(clust_b)):
-			coef_here = jaccard_coef(clust_a[i], clust_b[i])
+			coef_here = jaccard_coef(all_dict[str(clust_a[i])], all_dict[str(clust_b[j])])
 			if (coef_here > max_):
 				max_ = coef_here
 			if (coef_here < min_):
@@ -37,5 +35,33 @@ for each in df["Topics"]:
 	all_dict[str(counter)] = topics_here
 
 topics = set(topics)
+
+
+node_list = [[x] for x in range(1, counter + 1)]
+
+iter_ = 1
+print()
+while(1):
+	print("iter_no: "+str(iter_)+", size: "+str(len(node_list)), end="\r")
+	iter_ += 1
+	if len(node_list) <= 9:
+		break
+	min_dist = float('inf')
+
+	choice_1 = 0
+	choice_2 = 0
+
+	for i in range(len(node_list)):
+		for j in range(i+1, len(node_list)):
+			dist_here = distance(node_list[i], node_list[j])
+			if min_dist < dist_here:
+				min_dist = dist_here
+				choice_1 = i
+				choice_2 = j
+
+	node_list[choice_1].extend(node_list[choice_2])
+	node_list = node_list[:j] + node_list[j+1:]
+
+print()
 
 pu.db
