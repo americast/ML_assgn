a449b94f4422d859195ce79e7510a2c5eb264f14
americast <sayan.sinha@iitkgp.ac.in>
2019-03-31 10:24:57 +0800

    Update

--- Assgn3/NMI.py
+++ Assgn3/NMI.py
@@ -4,7 +4,7 @@ import pudb
 
 df = pd.read_csv("AAAI.csv")
 
-import part1_mod
+import part1
 import part2
 import part3
 import warnings
@@ -32,7 +32,7 @@ def entropy_class(arr):
 	return compute_entropy(list(df_here["High-Level Keyword(s)"].value_counts()))
 	# pu.db
 
-def NMF(clus):
+def NMI(clus):
 	# h_y = entropy_class(clus)
 	h_c = entropy_clus(clus)
 
@@ -49,17 +49,17 @@ def NMF(clus):
 	# pu.db
 	return 2 * float(I_y_c) / (h_y + h_c)
 
+print("Performing complete hierarchical clustering: ")
 clus_1 = part1_mod.hier_clus(df)
+NMI_1 = NMI(clus_1)
+print("NMI: "+str(NMI_1)+"\n")
+
+print("Performing single hierarchical clustering: ")
 clus_2 = part1_mod.hier_clus(df, "single")
+NMI_2 = NMI(clus_2)
+print("NMI: "+str(NMI_2)+"\n")
+
+print("Performing graph clustering (with threshold at 0.1): ")
 clus_3 = part2.graph_clus(df, 0.1)
-# clus_4 = part3.high_level_clus(df)
-
-# NMF_4 = NMF(clus_4)
-NMF_1 = NMF(clus_1)
-NMF_2 = NMF(clus_2)
-NMF_3 = NMF(clus_3)
-
-print("NMF of:\nHierarchical clustering (complete): "+str(NMF_1)+"\nHierarchical clustering (single): "+str(NMF_2))
-print("Graph clustering: "+str(NMF_3))
-
-# pu.db
+NMI_3 = NMI(clus_3)
+print("NMI: "+str(NMI_3)+"\n")
--- Assgn3/part1.py
+++ Assgn3/part1.py
@@ -5,26 +5,23 @@ import pudb
 def jaccard_coef(a, b):
 	inter = len(set(a).intersection(b))
 	union = len(set(a).union(set(b)))
-	# print(a)
-	# print(b)
 	return float(inter) / union
 
-# def distance(clust_choice_a, clust_choice_b, all_dict, node_list, node_lens, linkage="complete"):
-# 	max_ = float('-inf')
-# 	min_ = float('inf')
-# 	for i in range(node_lens[clust_choice_a]):
-# 		for j in range(node_lens[clust_choice_b]):
-# 			coef_here = jaccard_coef(all_dict[str(node_list[clust_choice_a][i])], all_dict[str(node_list[clust_choice_b][j])])
-# 			print("coef here: ", coef_here)
-# 			if (coef_here > max_):
-# 				max_ = coef_here
-# 			if (coef_here < min_):
-# 				min_ = coef_here
+def proximity(clust_a, clust_b, node_lens_i, node_lens_j, all_dict, linkage="complete"):
+	max_ = float('-inf')
+	min_ = float('inf')
+	for i in range(node_lens_i):
+		for j in range(node_lens_j):
+			coef_here = jaccard_coef(all_dict[str(clust_a[i])], all_dict[str(clust_b[j])])
+			if (coef_here > max_):
+				max_ = coef_here
+			if (coef_here < min_):
+				min_ = coef_here
 
-# 	if linkage == "complete":
-# 		return max_
-# 	else:
-# 		return min_
+	if linkage == "complete":
+		return min_
+	else:
+		return max_
 
 def unique(list1): 
 	# intilize a null list 
@@ -37,8 +34,7 @@ def unique(list1):
 	        unique_list.append(x) 
 	return unique_list
 
-
-def hier_clus(df, linkage="complete"):
+def hier_clus(df, linkage = "complete"):
 	topics = []
 	all_dict = {}
 	counter = 0
@@ -53,31 +49,19 @@ def hier_clus(df, linkage="complete"):
 
 
 	node_list = [[x] for x in range(1, counter + 1)]
+	node_lens = [1 for x in range(counter)]
 
 	for i in range(len(node_list)):
-		for j in range(len(node_list)):
+		for j in range(len(node_list) - 1):
 			node_list[i].append(-1)
-
-	node_lens = [1 for x in range(len(node_list))]
-
-	prox_mat = np.zeros((len(node_list), len(node_list)))
-
-	tot_count = len(node_list)
-
-	for i in range(tot_count):
-		for j in range(i + 1, tot_count):
-			prox_mat[i, j] = jaccard_coef(all_dict[str(node_list[i][0])], all_dict[str(node_list[j][0])])
-			prox_mat[j, i] = prox_mat[i, j]
 
 	iter_ = 1
 	print()
 	while(1):
-		print("iter_no: "+str(iter_)+", size: "+str(tot_count)+"    ", end="\r")
+		print("iter_no: "+str(iter_)+", size: "+str(len(node_list))+"   ", end="\r")
 		iter_ += 1
-		if tot_count <= 9:
+		if len(node_list) <= 9:
 			break
-		tot_count-=1
-
 		min_dist = float('-inf')
 
 		choice_1 = 0
@@ -85,76 +69,44 @@ def hier_clus(df, linkage="complete"):
 
 		for i in range(len(node_list)):
 			for j in range(i+1, len(node_list)):
-				if node_list[i] == node_list[j]:
-					# print("Here")
-					continue
-				try:
-					dist_here = prox_mat[i, j]
-				except:
-					pu.db
+				dist_here = proximity(node_list[i], node_list[j], node_lens[i], node_lens[j], all_dict, linkage)
 				if min_dist < dist_here:
 					min_dist = dist_here
 					choice_1 = i
 					choice_2 = j
 
-		choice_1_s = []
-		choice_2_s = []
-
-		for k in range(len(node_list)):
-			if node_list[k] == node_list[choice_1]:
-				choice_1_s.append(k)
-			if node_list[k] == node_list[choice_2]:
-				choice_2_s.append(k)
-		# print("choice_1_s: ", choice_1_s)
-		# print("choice_2_s: ", choice_2_s)
-
-		for i in range(len(node_list)):
-			if i not in choice_1_s and i not in choice_2_s:
-				if linkage == "complete":
-					prox_mat[choice_1, i] = max(prox_mat[choice_1, i], prox_mat[choice_2, i])
-				else:
-					prox_mat[choice_1, i] = min(prox_mat[choice_1, i], prox_mat[choice_2, i])
-
-				for l in choice_2_s:
-					prox_mat[l, i] = prox_mat[choice_1, i]
-					prox_mat[i, l] = prox_mat[choice_1, i]
-
-				for l in choice_1_s:
-					prox_mat[i, l] = prox_mat[choice_1, i]
-
-
-		# if (iter_ == 138):
-		# 	pu.db
 
 		for each_item in node_list[choice_2]:
 			if each_item == -1:
 				break
 			try:
 				node_list[choice_1][node_lens[choice_1]] = each_item
-				node_lens[choice_1] += 1
 			except:
 				pu.db
+			node_lens[choice_1] += 1
 
-		for l in choice_1_s:
-			node_list[l] = node_list[choice_1]
-			node_lens[l] = node_lens[choice_1]
+		# node_list[choice_1].extend(node_list[choice_2])
+		node_list.append(node_list[choice_1])
+		node_lens.append(node_lens[choice_1])
+		node_list = node_list[:choice_1] + node_list[choice_1+1:]
+		node_lens = node_lens[:choice_1] + node_lens[choice_1+1:]
+		node_list = node_list[:choice_2 - 1] + node_list[choice_2:]
+		node_lens = node_lens[:choice_2 - 1] + node_lens[choice_2:]
 
-
-		for l in choice_2_s:
-			node_list[l] = node_list[choice_1]
-			node_lens[l] = node_lens[choice_1]
-
-
-	print()
+	print("\n")
 
 	node_list = unique(node_list)
 
 	for i in range(len(node_list)):
 		node_list[i] = node_list[i][:np.argmin(node_list[i])]
 
+	# print("Final length: "+str(len(node_list)))
+
 	return node_list
 
-if __name__=="__main__":
+	pu.db
+
+if __name__ == "__main__":
 	df = pd.read_csv("AAAI.csv")
 	print("Choose linkage type:\n1) Single\n2) Complete\n")
 	choice = int(input("Enter choice: "))
