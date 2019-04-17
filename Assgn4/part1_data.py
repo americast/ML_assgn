a81d1ee8da22407ac6977bcc609d7056907191bc
americast <sayan.sinha@iitkgp.ac.in>
2019-04-18 03:48:37 +0530

    Initial commit

---
+++ Assgn4/part1_data.py
@@ -0,0 +1,65 @@
+from nltk.tokenize import RegexpTokenizer
+import pudb
+
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+tokenizer = RegexpTokenizer(r'\w+')
+
+res = []
+docs = []
+all_words = {}
+
+f = open("Assignment_4_data.txt", "r")
+
+while(1):
+	line = f.readline()
+	if not line:
+		break
+	num_chars = len(line.split()[0])
+	type_ = line[:num_chars]
+	text = line[num_chars:]
+	tokens = tokenizer.tokenize(text.lower())
+
+	tokens = [w for w in tokens if not w in stop_words] 
+
+	if (type_=="spam"):
+		res.append(1)
+	else:
+		res.append(0)
+
+	docs.append(tokens)
+
+	for word in tokens:
+		if word not in all_words:
+			all_words[word] = 1
+		else:
+			all_words[word] += 1
+
+all_words_list = []
+for each in all_words:
+	all_words_list.append([each, all_words[each]])
+
+all_words_list.sort(reverse = True, key = lambda x: x[1])
+all_words_list = all_words_list[:500]
+all_words_list = [x[0] for x in all_words_list]
+
+indices = {}
+count = 0
+
+for each in all_words_list:
+	indices[each] = count
+	count+=1
+
+docs_sparse = []
+
+for each in docs:
+	print()
+	each_sparse = [0 for x in range(len(all_words))]
+	for e in each:
+		if e in all_words_list:
+			print(indices[e])
+			each_sparse[indices[e]] = 1
+	docs_sparse.append(each_sparse)
+
+
+pu.db
